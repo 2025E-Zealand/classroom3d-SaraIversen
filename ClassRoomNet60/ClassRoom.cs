b9f593211903dce67f4ca8c3c12739871318110d
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-08-27 12:24:36 +0200

    New class: ClassRoom

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        public string Name { get; private set; }
+        public List<Student> StudentList { get; private set; }
+        public DateTime SemesterStart { get; private set; }
+
+        public ClassRoom(string name, List<Student> studentList, DateTime semesterStart)
+        {
+            Name = name;
+            StudentList = studentList;
+            SemesterStart = semesterStart;
+        }
+    }
+}
