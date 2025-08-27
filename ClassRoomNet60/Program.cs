99e5099c3fde19b52be30f47948a0f9de108aea3
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-08-27 12:28:40 +0200

    Created classroom instance

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ClassRoomNet60;
+
 Console.WriteLine("Hello, World!");
+
+ClassRoom classRoom = new ClassRoom(
+    name: "3Q Beginning of the semester",
+    studentList: new List<Student>
+    {
+        new Student("Person1", 4, 17),
+        new Student("Person2", 2, 14),
+        new Student("Person3", 12, 24)
+    },
+    semesterStart: new DateTime(2019, 8, 26)
+);
